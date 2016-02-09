2877fe833fbff865e34e98d8f126fe61d6f0431a
harold <harold@harold-Compaq-Presario-CQ40-Notebook-PC>
2016-02-08 20:08:57 -0500

    tercera version validando metodos

--- Programa1/src/programa1/Programa1.java
+++ Programa1/src/programa1/Programa1.java
@@ -6,10 +6,10 @@ public class Programa1 {
 
    
     public static void main(String[] args) {
-        int a= 5+5;
-        int suma=a;
+       
         
-        System.out.println("La suma es "+suma);
     }
-    
+    public void metodo(){
+        System.out.println("ESTO ES UN METODO   ");
+    }
 }
